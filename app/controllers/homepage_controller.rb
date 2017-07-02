cc7aa9ca6f5aff9010d198d8bad344a6ffa672a0
Pedro Henrique Farias Cassiano <pf.cassiano@gmail.com>
2017-07-02 20:48:29 -0300

    graficos

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -3,7 +3,23 @@ class HomepageController < ApplicationController
   end
   
   def result
-    @ini = params[:date_begin]
-    @fim = params[:date_end]
+    @date_begin = Date.new(params[:year_begin].to_i,params[:month_begin].to_i,1)
+    @date_end = Date.new(params[:year_end].to_i,params[:month_end].to_i,1)
+    @media_temp = Hash.new
+    @desvio_temp = Hash.new
+    @media_dewp = Hash.new
+    @desvio_dewp = Hash.new
+    @media_w = Hash.new
+    @desvio_w = Hash.new
+    
+    months = Month.all.months_between(@date_begin,@date_end)
+    months.each do |m|
+      @media_temp[m.mes_ano] = m.media_temp
+      @desvio_temp[m.mes_ano] = m.desvio_temp
+      @media_dewp[m.mes_ano] = m.media_dewp
+      @desvio_dewp[m.mes_ano] = m.desvio_dewp
+      @media_w[m.mes_ano] = m.media_w
+      @desvio_w[m.mes_ano] = m.desvio_w
+    end
   end
 end
--- app/models/month.rb
+++ app/models/month.rb
@@ -1,2 +1,3 @@
 class Month < ApplicationRecord
+  scope :months_between, lambda {|start_date, end_date| where("mes_ano >= ? AND mes_ano <= ?", start_date, end_date )}
 end
