68b2394174b351cf58528ea9fa16807671fc4257
Pedro Henrique Farias Cassiano <pf.cassiano@gmail.com>
2017-07-02 23:19:11 -0300

    script

--- run_hadoop.sh
+++ run_hadoop.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
-for i in {1932..2017}
+HADOOP_HOME=$HADOOP_HOME
+MAP_REDUCE_APP_PATH=/home/pedro/workspace/ep2-map-reduce
+DATA_PATH=/home/pedro/Documents/dados-meteorologicos/ftp.ncdc.noaa.gov/pub/data/gsod
+
+
+for i in {1929..2017}
 do
-  hadoop jar ~/workspace/ep2-map-reduce/target/ep2-map-reduce-1.0-SNAPSHOT.jar br.usp.sd.App ~/Documents/dados-meteorologicos/ftp.ncdc.noaa.gov/pub/data/gsod/$i hadoop/$i
+  $HADOOP_HOME/bin/hadoop jar $MAP_REDUCE_APP_PATH/target/ep2-map-reduce-1.0-SNAPSHOT.jar br.usp.sd.App $DATA_PATH/$i hadoop/$i
 done
+
+# CASO ESTEJA USANDO HDFS UTILIZE A SEGUINTE 
+#$HADOOP_HOME/bin/hdfs dfs -get hadoop hadoop
